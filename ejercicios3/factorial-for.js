fcc91910309e28e251b70bad7203fc9f5bbeef20
Shabull XYZ <shabull1997@gmail.com>
2022-09-12 10:17:10 -0300

    Ejercicios 3

---
+++ ejercicios3/factorial-for.js
@@ -0,0 +1,7 @@
+//Bucle For
+
+let factorial = 1
+for (let i = 10; i > 0; i--) {
+    factorial *= i
+}
+console.log(factorial)
---
+++ ejercicios3/factorial-while.js
@@ -0,0 +1,11 @@
+//Factorial While
+
+
+let factorial = 1
+let num = 10
+while (num > 1) {
+    factorial *= num
+    num--
+}
+console.log(factorial) 
+
