b2c4cc3ad19b0ab5bb894ae800b1d11612bb14ca
Shabull XYZ <shabull1997@gmail.com>
2022-09-14 15:23:44 -0300

    Ejercicios 5

---
+++ ejercicio5numeros.js
@@ -0,0 +1,10 @@
+let alturacm = 169;
+let alturamt = 1.69;
+let pesokg = 63.51;
+let altredondeada = Math.ceil(alturamt)
+let pesokgredon = Math.floor(pesokg);
+
+//El maximo valor que se puede obtener en JS + 1
+
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(sonIguales)
